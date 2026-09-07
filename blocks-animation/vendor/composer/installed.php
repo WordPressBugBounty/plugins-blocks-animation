8a9f013fd0ab53cfd8351530ce3ade79c797ae5d
Sean Murphy <sean@iamseanmurphy.com>
2026-09-07 15:53:00 +0000

    3.2.4

--- blocks-animation/vendor/composer/installed.php
+++ blocks-animation/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/blocks-animation',
-        'pretty_version' => '3.2.2',
-        'version' => '3.2.2.0',
+        'pretty_version' => '3.2.4',
+        'version' => '3.2.4.0',
         'reference' => null,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'codeinwp/blocks-animation' => array(
-            'pretty_version' => '3.2.2',
-            'version' => '3.2.2.0',
+            'pretty_version' => '3.2.4',
+            'version' => '3.2.4.0',
             'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.58',
-            'version' => '3.3.58.0',
-            'reference' => 'd6807c0b7308e323bd77cced667dee3f2d5e6a82',
+            'pretty_version' => '3.3.61',
+            'version' => '3.3.61.0',
+            'reference' => '9fe698b52dec768a0dd8b500fb51efe40962ee99',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
